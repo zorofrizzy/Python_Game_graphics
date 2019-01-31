17a960bf986bf7af0130256ef20a5d91cd361427
Zorawar <44931523+zorofrizzy@users.noreply.github.com>
2019-01-31 16:17:09 +0530

    Add files via upload

---
+++ pygame1.py
@@ -0,0 +1,39 @@
+import pygame
+import time
+pygame.init()
+
+win = pygame.display.set_mode((600,500))
+
+pygame.display.set_caption("BAT GAME")
+
+x = 50
+y = 50
+width = 4
+height = 5
+speed = 20
+
+run= True
+while run:
+    pygame.time.delay(100)
+    #time.sleep(2)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys [pygame.K_LEFT] :
+        x-= speed
+    if keys [pygame.K_RIGHT] :
+        x+= speed
+    if keys [pygame.K_UP] :
+        y-= speed
+    if keys [pygame.K_DOWN] :
+        y+= speed    
+
+    win.fill((0,0,0))        
+    pygame.draw.rect(win, (227,11,93), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
+            
